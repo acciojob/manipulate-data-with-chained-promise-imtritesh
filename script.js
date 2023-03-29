43679c42c8f7a444c9c1eaa5e2bcc0ec99c54d74
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-03-29 16:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let arr = [1, 2, 3, 4];
+setTimeout(()=>{
+	setTimeout(()=>{
+		document.getElementById("output").innerHTML=arr.filter(ele => ele%2===0);
+		setTimeout(()=>{
+		document.getElementById("output").innerHTML=arr.filter(ele => ele%2===0).map(ele => ele*2);
+		},2000)
+	},1000)
+},3000)
